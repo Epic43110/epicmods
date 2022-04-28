570ccc8b4875d3e77bc387667f3951a89c61ef58
Epic43110 <82621127+Epic43110@users.noreply.github.com>
2022-04-28 18:36:54 -0500

    Delete remove-msg.js

--- remove-msg.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(e) {
-    var key = e.keyCode || e.which;
-    switch (key) {
-        case 27:
-            document.getElementById('closeMsg').click();
-    }
-});
